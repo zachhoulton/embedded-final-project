d117cde835151b82abe688d6127047a6453e7276
Zachary Houlton <houltonzachary@gmail.com>
2025-04-17 10:30:24 -0500

    initial

---
+++ Debug/Arabic.d
@@ -0,0 +1,7 @@
+# FIXED
+
+Arabic.o: C:/MSPM0_ValvanoWare/inc/Arabic.c \
+ C:/MSPM0_ValvanoWare/inc/Arabic.h \
+ C:/MSPM0_ValvanoWare/inc/ST7735.h
+C:/MSPM0_ValvanoWare/inc/Arabic.h:
+C:/MSPM0_ValvanoWare/inc/ST7735.h:
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,119 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../mspm0g3507.cmd 
+
+C_SRCS += \
+../ADC.c \
+C:/MSPM0_ValvanoWare/inc/ADC1.c \
+C:/MSPM0_ValvanoWare/inc/Arabic.c \
+C:/MSPM0_ValvanoWare/inc/Clock.c \
+C:/MSPM0_ValvanoWare/inc/DAC5.c \
+../JoyStick.c \
+../LED.c \
+../Lab9Main.c \
+C:/MSPM0_ValvanoWare/inc/LaunchPad.c \
+C:/MSPM0_ValvanoWare/inc/SPI.c \
+C:/MSPM0_ValvanoWare/inc/ST7735.c \
+../SmallFont.c \
+../Sound.c \
+../Switch.c \
+C:/MSPM0_ValvanoWare/inc/TExaS.c \
+C:/MSPM0_ValvanoWare/inc/Timer.c 
+
+C_DEPS += \
+./ADC.d \
+./ADC1.d \
+./Arabic.d \
+./Clock.d \
+./DAC5.d \
+./JoyStick.d \
+./LED.d \
+./Lab9Main.d \
+./LaunchPad.d \
+./SPI.d \
+./ST7735.d \
+./SmallFont.d \
+./Sound.d \
+./Switch.d \
+./TExaS.d \
+./Timer.d 
+
+OBJS += \
+./ADC.o \
+./ADC1.o \
+./Arabic.o \
+./Clock.o \
+./DAC5.o \
+./JoyStick.o \
+./LED.o \
+./Lab9Main.o \
+./LaunchPad.o \
+./SPI.o \
+./ST7735.o \
+./SmallFont.o \
+./Sound.o \
+./Switch.o \
+./TExaS.o \
+./Timer.o 
+
+OBJS__QUOTED += \
+"ADC.o" \
+"ADC1.o" \
+"Arabic.o" \
+"Clock.o" \
+"DAC5.o" \
+"JoyStick.o" \
+"LED.o" \
+"Lab9Main.o" \
+"LaunchPad.o" \
+"SPI.o" \
+"ST7735.o" \
+"SmallFont.o" \
+"Sound.o" \
+"Switch.o" \
+"TExaS.o" \
+"Timer.o" 
+
+C_DEPS__QUOTED += \
+"ADC.d" \
+"ADC1.d" \
+"Arabic.d" \
+"Clock.d" \
+"DAC5.d" \
+"JoyStick.d" \
+"LED.d" \
+"Lab9Main.d" \
+"LaunchPad.d" \
+"SPI.d" \
+"ST7735.d" \
+"SmallFont.d" \
+"Sound.d" \
+"Switch.d" \
+"TExaS.d" \
+"Timer.d" 
+
+C_SRCS__QUOTED += \
+"../ADC.c" \
+"C:/MSPM0_ValvanoWare/inc/ADC1.c" \
+"C:/MSPM0_ValvanoWare/inc/Arabic.c" \
+"C:/MSPM0_ValvanoWare/inc/Clock.c" \
+"C:/MSPM0_ValvanoWare/inc/DAC5.c" \
+"../JoyStick.c" \
+"../LED.c" \
+"../Lab9Main.c" \
+"C:/MSPM0_ValvanoWare/inc/LaunchPad.c" \
+"C:/MSPM0_ValvanoWare/inc/SPI.c" \
+"C:/MSPM0_ValvanoWare/inc/ST7735.c" \
+"../SmallFont.c" \
+"../Sound.c" \
+"../Switch.c" \
+"C:/MSPM0_ValvanoWare/inc/TExaS.c" \
+"C:/MSPM0_ValvanoWare/inc/Timer.c" 
+
+
